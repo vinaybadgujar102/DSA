b20dcd573b286380b881114ed1579a0eec0970fc
Vinay Arvind Badgujar <90821253+vinaybadgujar102@users.noreply.github.com>
2022-10-31 22:16:30 +0530

    Index of duplicate elements in array

--- Recursion/Problems/LinearSearch.java
+++ Recursion/Problems/LinearSearch.java
@@ -1,9 +1,12 @@
+import java.util.ArrayList;
+
 public class LinearSearch {
     public static void main(String[] args) {
-        int[] arr = {1, 2, 3, 4, 5};
+        int[] arr = {1, 2, 3, 4, 5, 5};
         int target = 5;
         System.out.println(linearSearch(arr, target, 0));
         System.out.println(linearSearch2(arr, target, 0));
+        System.out.println(findAllIndex(arr, target, 0, new ArrayList<>()));
     }
 
     //returns boolean
@@ -23,6 +26,16 @@ public class LinearSearch {
         else{
             return linearSearch2(arr, target, index + 1);
         }
+    }
+
+    //finds and stores the index of target element in Arraylist
+    static ArrayList<Integer> findAllIndex(int[] arr, int target, int index, ArrayList<Integer> list){
+        if(index == arr.length) return list;
+
+        if(arr[index]==target){
+            list.add(index);
+        }
+        return findAllIndex(arr, target, index + 1, list);
 
     }
 }
